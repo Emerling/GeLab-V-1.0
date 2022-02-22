011674dd6a71eac7808030ee9c5843a63f3a1921
Emerling <emerling2001@gmail.com>
2022-02-22 14:13:04 -0400

    Actualizacion lab-1

--- app/Views/body.php
+++ app/Views/body.php
@@ -2,24 +2,24 @@
     <div class="mdb-page-content text-center page-intro bg-light">
         <div class="card-group">
             <div class="card text-white bg-secondary mb-0">
-                <i class="fas fa-id-card fa-2x card-img-top mt-3  "></i>
+                <i class="fas fa-id-card fa-3x card-img-top mt-3  "></i>
 
                 <div class="card-body">
                     <h6 class="card-title">Paciente</h6>
-                    <p class="card-text">
+                  <!--   <p class="card-text">
                        Gestionar Pacientes.
-                    </p> 
+                    </p>  -->
                     <a href="#" class="btn btn-primary mb-">Aceptar</a>
                 </div>
 
             </div>
             <div class="card text-white bg-success mb-0">
-                <i class="fas fa-vial fa-2x card-img-top mt-3 "></i>
+                <i class="fas fa-vial fa-3x card-img-top mt-3 "></i>
                 <div class="card-body">
                     <h6 class="card-title">Exámenes de Laboratorio</h6>
-                    <p class="card-text">
+                   <!--  <p class="card-text">
                         Gestionar Exámenes.
-                    </p> 
+                    </p>  -->
                   
                     <a href="#" class="btn btn-primary">Aceptar</a>
 
@@ -27,24 +27,24 @@
 
             </div>
             <div class="card text-white bg-info mb-0">
-                <i class="fas fa-clipboard-list fa-2x card-img-top mt-3 "></i>
+                <i class="fas fa-clipboard-list fa-3x card-img-top mt-3 "></i>
                 <div class="card-body">
                     <h6 class="card-title">Resultados de Laboratorio</h6>
-                   <p class="card-text">
+                   <!-- <p class="card-text">
                         Buscar entre los registros de exámenes realizados.
-                    </p> 
+                    </p>  -->
                     <a href="#" class="btn btn-primary">Aceptar</a>
 
                 </div>
 
             </div>
             <div class="card text-white bg-danger mb-0">
-                <i class="fas fa-calculator fa-2x card-img-top mt-3 "></i>
+                <i class="fas fa-calculator fa-3x card-img-top mt-3 "></i>
                 <div class="card-body">
                     <h6 class="card-title">Ordenes-Presupuestos-Facturación</h6>
-                    <p class="card-text">
+                   <!--  <p class="card-text">
                        Gestionar los pedidos solicitados.
-                    </p> 
+                    </p>  -->
                         <a href="#" class="btn btn-primary">Aceptar</a>
 
                 </div>
@@ -56,7 +56,7 @@
     </div>
 
     <!-- Background image -->
-    <div class="p-0 bg-image" style="background-image: url('img/pagina.jpg');height: 626px;">
+    <div class="p-0 bg-image" style="background-image: url('img/pagina.jpg');height: 310px;">
         <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
             <div class="d-flex justify-content-center align-items-center h-100">
                 <img class="img-fluid" style="max-width:60%;" src="img/logo-lab.png" alt="Logo">
--- app/Views/piedepagina.php
+++ app/Views/piedepagina.php
@@ -1,14 +1,15 @@
 
-<footer class="text-center font-small fixed-bottom text-white unique-color-dark" style="background-color: #343546;">
+<footer class="text-white-50 text-center text-lg-start" style="background-color:#1c2331;">
   <!-- Grid container -->
   <a  rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia Creative Commons" style="border-width:0" src="img/licencia.png" /></a><br />Este software está bajo una <a class="text-info" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Licencia Creative Commons Atribución-NoComercial-CompartirIgual 4.0 Internacional</a>.
  
   <!-- Copyright -->
-  <div class="text-center p-1" style="background-color: rgba(1, 1, 1, 0.5);">
-    App desarrollada por
-    <a class="text-info" href="https://twitter.com/emerling/">@Emerling</a> - DER System's bajo el Código: D-2112-01 para L.C.C.M.
+  <div class="text-center p-1"  style="background-color: rgba(0, 0, 0, 0.2);">
+ 
+  App desarrollada por
+    <a class="text-info" href="https://twitter.com/emerling/">@Emerling</a> - DER System's bajo el Código: D-2112-01 Autorización permitida para L.C.C.M.
   </div>
-  <!-- Copyright -
+  <!-- Copyright -->
 </footer>
 
 
